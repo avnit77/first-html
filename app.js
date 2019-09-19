f6c79dcacfb23ab301ea9ac36a1aec3d894a30f1
Tali Avni <tali@aristo7.com>
2019-09-18 20:58:14 -0700

    message

---
+++ app.js
@@ -0,0 +1,32 @@
+function showAlert() {
+    alert('youclicked the button');
+}
+
+function logHello() {
+    console.log('Hello')
+}
+
+const shark ={
+    name: 'jaws',
+    color: 'grey',
+    weight: 500,
+}
+
+console.log(shark.name);
+console.log(shark.color);
+
+if(shark.name === 'jaws') {
+    console.log('dun dun');   
+} else if (shark.name === 'jaws 2') {
+    console.log('dun dun');
+} else {
+    console.log('dun dun dun dun dun');
+}
+
+// count up to five
+const veggies = ['carrot', 'potato', 'turnip']
+for (let i = 0; i <= 5; i++) 
+if (i === 1) {
+    console.log(veggies[i])
+}
+
